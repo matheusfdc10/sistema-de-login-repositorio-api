97a6ee0aaf5947ed2a46e0983c80434698c856de
matheusfdc10 <64614285+matheusfdc10@users.noreply.github.com>
2023-01-01 15:47:35 -0300

    at

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,5 @@
 import 'dotenv/config'
 
 export default {
-    url: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sgerfeh.mongodb.net/?retryWrites=true&w=majority`
+    url: process.env.DB_URL
 }
